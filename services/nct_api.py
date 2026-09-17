f1ca11c37cb19f53c2da8ed290cb9abc53cbc944
Hoang Anh <doviethoanganh07@gmail.com>
2026-09-18 05:23:57 +0700

    Update nct_api.py

--- services/nct_api.py
+++ services/nct_api.py
@@ -2,7 +2,7 @@ import requests
 
 
 NCT_API = "https://graph.nhaccuatui.com/api/v1/search/song"
-
+TIMEOUT = 10
 
 def search_music(keyword, pagesize=10):
 
@@ -13,9 +13,13 @@ def search_music(keyword, pagesize=10):
         "correct": "true",
     }
 
-    response = requests.post(
-        NCT_API,
-        params=params
-    )
-
-    return response.json()
+    try:
+        response = requests.post(
+            NCT_API,
+            params=params,
+            timeout=TIMEOUT
+        )
+        
+        return response.json()
+    except:
+        return None
